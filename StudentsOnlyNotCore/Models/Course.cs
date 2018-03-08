8c4d2bb48622db438eee8fba2e8ab03baf32767d
studentboy87 <thomasjedwards87@gmail.com>
2018-03-08 06:23:08 +0000

    Models and config

---
+++ StudentsOnlyNotCore/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsOnlyNotCore.Models
+{
+    public enum Grade
+    {
+        A,B,C,D,F
+    }
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
